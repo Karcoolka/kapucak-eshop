c89e5d5817d3b5f784b6abfa40643aba987d500f
Adela Simkova <adela@sherpadevs.com>
2026-02-07 23:23:26 +0100

    ShadCN UI, layout, constants

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
+
+const inter = Inter({subsets: ["latin"]});
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
