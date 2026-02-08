9511cf1378c988c4396ef4ea390235d77f9fb052
Adela Simkova <adela@sherpadevs.com>
2026-02-08 11:15:29 +0100

    product list implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const HomePage = () => {
-  return <>Prostore</>;
+  return <>
+  <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+  </>;
 };
 
 export default HomePage;
