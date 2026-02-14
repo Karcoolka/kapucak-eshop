907c5cdf67223b86366e313ef96fba273ca76174
Adela Simkova <adela@sherpadevs.com>
2026-02-14 07:10:45 +0100

    PLACE ORDER PAGE

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema } from "@/lib/validators";
+import { OrderItem } from "@/lib/generated/prisma";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -10,5 +11,13 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
-export type Order = z.infer<typeof insertOrderSchema>;
-export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: Boolean;
+    paidAt: Date | null;
+    isDelivered: Boolean;
+    deliveredAt: Date | null;
+    orderItems: OrderItem[];
+    user: { name: string; email: string };
+  };
