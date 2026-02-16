b67f95579af559033fe8a5e1c48aa586413b51d7
Adela Simkova <adela@sherpadevs.com>
2026-02-16 09:31:03 +0100

    order page, pagination

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -1,63 +1,77 @@
-import Link from 'next/link';
-import { auth } from '@/auth';
-import { Button } from '@/components/ui/button';
+import Link from "next/link";
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+} from "@/components/ui/dropdown-menu";
 
 const UserButton = async () => {
-    const session = await auth();
-    if (!session)
-      return (
-        <Link href='/api/auth/signin'>
-          <Button>Sign In</Button>
-        </Link>
-      );
-    
-    const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? '';
-      
-    return <div className='flex gap-2 items-center'>
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className='flex items-center'>
-          <Button
-            variant='ghost'
-            className='relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-300'
-          >
-            {firstInitial}
-          </Button>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-56' align='end' forceMount>
-        <DropdownMenuLabel className='font-normal'>
-          <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>
-              {session.user?.name}
-            </p>
-            <p className='text-xs leading-none text-muted-foreground'>
-              {session.user?.email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-  
-        <DropdownMenuItem asChild className='p-0 mb-1'>
-          <Link href='/api/auth/signout?callbackUrl=/' className='block w-full'>
+  const session = await auth();
+  if (!session)
+    return (
+      <Link href="/api/auth/signin">
+        <Button>Sign In</Button>
+      </Link>
+    );
+
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "";
+
+  return (
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
             <Button
-              type='button'
-              className='w-full py-4 px-2 h-4 justify-start'
-              variant='ghost'
+              variant="ghost"
+              className="relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-300"
             >
-              Sign Out
+              {firstInitial}
             </Button>
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </div>;
-  };
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/profile">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/orders">
+              Order History
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild className="p-0 mb-1">
+            <Link
+              href="/api/auth/signout?callbackUrl=/"
+              className="block w-full"
+            >
+              <Button
+                type="button"
+                className="w-full py-4 px-2 h-4 justify-start"
+                variant="ghost"
+              >
+                Sign Out
+              </Button>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
 
 export default UserButton;
