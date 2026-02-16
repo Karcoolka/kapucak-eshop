447d1277ae2f71c5c2e393c2987d6e6f37251b0c
Adela Simkova <adela@sherpadevs.com>
2026-02-16 19:40:53 +0100

    protected admin routes

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,24 @@
+import { Button
+
+ } from "@/components/ui/button";import { Metadata } from "next";
+import Link from "next/link";
+
+
+
+ export const metadata: Metadata = {
+    title: "Unauthorized",
+ };
+
+ const Unauthorized = () => {
+    return (
+        <div className="container mx-auto flex flex-col items-center justify-center space-y-4 h-[calc(100vh-200px)]">
+            <h1 className="h1-boldtext-4xl">Unauthorized Access</h1>
+            <p className="text-muted-foreground">You are not authorized to access this page</p>
+            <Button asChild>
+                <Link href="/">Return Home</Link>
+            </Button>
+        </div>
+    );
+ };
+
+ export default Unauthorized;
