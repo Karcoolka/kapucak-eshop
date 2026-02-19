7310e4d269efe5a0d87011f90b546a43486ea95f
Adela Simkova <adela@sherpadevs.com>
2026-02-19 18:13:42 +0100

    prisma schema, Zod, type

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema, paymentResultSchema } from "@/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema, paymentResultSchema, insertReviewSchema } from "@/lib/validators";
 import { OrderItem } from "@/lib/generated/prisma";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -23,3 +23,9 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   };
 
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
